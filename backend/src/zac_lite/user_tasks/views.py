29510cba6d4d87b18e16d190060f8f25da573741
Sergei Maertens <sergei@maykinmedia.nl>
2021-01-26 17:04:16 +0100

    :pencil: Generate friendly operation summaries

--- backend/src/zac_lite/user_tasks/views.py
+++ backend/src/zac_lite/user_tasks/views.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.request import Request
@@ -8,6 +10,15 @@ from .serializers import UserLinkSerializer, UserTaskLink
 
 
 class UserLinkCreateView(APIView):
+    """
+    Create a frontend URL to execute a Camunda user task.
+
+    Given the Camunda `taskId`, an automatically-expiring signed URL is generated that
+    can be shared with the intended person to execute the task. That user does not need
+    to log in (using ADFS or otherwise) to be able to execute the task.
+    """
+
+    schema_summary = _("Create task user-link")
     authentication_classes = (TokenAuthentication,)
     serializer_class = UserLinkSerializer
 
