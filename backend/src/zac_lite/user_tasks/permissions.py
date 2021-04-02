49526116247e4b71ed0e3d067f831a8d5ae1af23
silvia <silvia@maykinmedia.nl>
2021-04-02 16:07:16 +0200

    :passport_control: [#5] Update TokenIsValid

--- backend/src/zac_lite/user_tasks/permissions.py
+++ backend/src/zac_lite/user_tasks/permissions.py
@@ -1,19 +1,34 @@
+from typing import Optional
+
 from django_camunda.camunda_models import Task
 from rest_framework import permissions, views
 from rest_framework.request import Request
 
 from .tokens import token_generator
 
+TOKEN_IN_BODY = "TOKEN_IN_BODY"
+TOKEN_IN_URL = "TOKEN_IN_URL"
+
 
 class TokenIsValid(permissions.BasePermission):
     token_kwarg = "token"
 
     def has_permission(self, request: Request, view: views.APIView) -> bool:
-        resolver_kwargs = request.resolver_match.kwargs
-        if self.token_kwarg not in resolver_kwargs:
+        if not self.get_token(request, view):
             return False
         return True
 
     def has_object_permission(self, request: Request, view: views.APIView, obj: Task):
-        token = request.resolver_match.kwargs[self.token_kwarg]
+        token = self.get_token(request, view)
         return token_generator.check_token(obj, token)
+
+    def get_token(self, request: Request, view: views.APIView) -> Optional[str]:
+        if view.token_location == TOKEN_IN_BODY:
+            data = request.data
+        else:
+            data = request.resolver_match.kwargs
+
+        if self.token_kwarg in data:
+            return data[self.token_kwarg]
+
+        return None
