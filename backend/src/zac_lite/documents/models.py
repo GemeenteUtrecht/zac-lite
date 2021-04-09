4f5158efde254131b7cbb5750b26a6a241db249f
silvia <silvia@maykinmedia.nl>
2021-04-09 17:35:19 +0200

    :construction: [#6] Default DRC service configuration

--- backend/src/zac_lite/documents/models.py
+++ backend/src/zac_lite/documents/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from privates.fields import PrivateMediaFileField
+from solo.models import SingletonModel
+from zgw_consumers.constants import APITypes
 
 
 class UploadedDocument(models.Model):
@@ -34,3 +36,21 @@ class UploadedDocument(models.Model):
 
     def __str__(self):
         return self.file_name
+
+
+class DocumentServiceConfig(SingletonModel):
+    primary_drc = models.ForeignKey(
+        "zgw_consumers.Service",
+        null=True,
+        on_delete=models.SET_NULL,
+        limit_choices_to={"api_type": APITypes.drc},
+        verbose_name=_("primary DRC"),
+        help_text=_("Document API where new documents will be created."),
+    )
+
+    class Meta:
+        verbose_name = _("document serivce configuration")
+        verbose_name_plural = _("document serivce configurations")
+
+    def __str__(self):
+        return "Default document service configuration"
