b9cd66a6f23ae2d097ed564c79ddf57cd13a5111
silvia <silvia@maykinmedia.nl>
2021-04-09 17:36:02 +0200

    :construction: [#6] First step document creation/update

--- backend/src/zac_lite/user_tasks/views.py
+++ backend/src/zac_lite/user_tasks/views.py
@@ -1,3 +1,7 @@
+import base64
+from datetime import date
+from typing import List
+
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from django.utils.translation import gettext_lazy as _
@@ -11,9 +15,11 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from zgw_consumers.models import Service
 
 from zac_lite.api.serializers import ErrorSerializer
 
+from ..documents.models import DocumentServiceConfig, UploadedDocument
 from .context import get_context
 from .data import UserTaskData, UserTaskLink
 from .parsers import NoUnderscoreBeforeNumberCamelCaseJSONParser
@@ -23,6 +29,7 @@ from .serializers import (
     UserLinkSerializer,
     UserTaskConfigurationSerializer,
 )
+from .zaak_documents import ZaakDocumentsContext
 
 
 class UserLinkCreateView(APIView):
@@ -110,13 +117,17 @@ class SubmitUserTaskView(APIView):
         )
         serializer.is_valid(raise_exception=True)
 
-        # Create the new documents
+        new_documents = self.create_new_documents(
+            serializer.validated_data["new_documents"], task_context
+        )
 
-        # Update existing documents
+        updated_documents = self.update_documents(
+            serializer.validated_data["replaced_documents"]
+        )
 
         # Complete user task in Camunda API
 
-        return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_200_OK)
 
     def get_object(self) -> Task:
         task_id = force_str(urlsafe_base64_decode(self.request.data["tidb64"]))
@@ -130,3 +141,60 @@ class SubmitUserTaskView(APIView):
         self.check_object_permissions(self.request, task)
 
         return task
+
+    # TODO parallelise
+    def create_new_documents(
+        self, documents: List, task_context: ZaakDocumentsContext
+    ) -> List[str]:
+        config = DocumentServiceConfig.get_solo()
+        default_document_service = config.primary_drc
+        if not default_document_service:
+            raise RuntimeError("No default DRC service configured.")
+
+        client = default_document_service.build_client()
+
+        new_documents_urls = []
+
+        for document in documents:
+            uploaded_document = UploadedDocument.objects.get(uuid=document["id"])
+            document_data = {
+                "informatieobjecttype": document["document_type"],
+                "bronorganisatie": task_context.zaak.bronorganisatie,
+                "creatiedatum": date.today().isoformat(),
+                "titel": uploaded_document.file_name,
+                # TODO: maybe the author should be an extra field of the UploadedDocument
+                "auteur": task_context.zaak.bronorganisatie,
+                "taal": "nld",
+                "inhoud": base64.b64encode(uploaded_document.content.read()).decode(
+                    "ascii"
+                ),
+            }
+            new_document = client.create(
+                resource="enkelvoudiginformatieobject", data=document_data
+            )
+            new_documents_urls.append(new_document["url"])
+
+        return new_documents_urls
+
+    # TODO parallelise
+    def update_documents(self, documents: List) -> List[str]:
+
+        updated_documents_urls = []
+
+        for document in documents:
+            client = Service.get_client(document["old"])
+
+            uploaded_document = UploadedDocument.objects.get(uuid=document["id"])
+            document_data = {
+                "titel": uploaded_document.file_name,
+                "inhoud": base64.b64encode(uploaded_document.content.read()).decode(
+                    "ascii"
+                ),
+            }
+
+            updated_document = client.update(
+                "enkelvoudiginformatieobject", url=document["old"], data=document_data
+            )
+            updated_documents_urls.append(updated_document["url"])
+
+        return updated_documents_urls
